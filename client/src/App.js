39abbfb8e431808af19103d7e4a6e08efa3fefcc
Chris Duong <ctd4wa@virginia.edu>
2020-05-20 14:46:27 -0400

    adding landing and register components with actions and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import PrivateRoute from './components/routing/PrivateRoute';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return(
+    <Provider store ={store}>
+      <Router>
+        <Fragment>
+
+        </Fragment>
+      </Router>
+    </Provider>
+  )};
+
+export default App;
