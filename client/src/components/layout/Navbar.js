7ad64ccaaa8597318d8ecfc1b947432a2cfd8c0a
Chris Duong <ctd4wa@virginia.edu>
2020-05-20 17:25:30 -0400

    user login and register pages along with landing page

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -45,7 +45,7 @@ const Navbar = ({ auth: {isAuthenticated, loading }, logout}) => {
       <nav className="navbar bg-dark">
         <h1>
           <Link to='/'>
-            <i className="fas fa-code"></i> EmptyPantry
+            <i className="fas fa-bread-slice"></i> EmptyPantry
           </Link>
         </h1>
         { !loading && (<Fragment>{ isAuthenticated ? authLinks : guestLinks }</Fragment>)}
