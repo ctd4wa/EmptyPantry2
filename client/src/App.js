712c6b20d279eea5ef26ee1d2ba00d6743f52031
Chris Duong <ctd4wa@virginia.edu>
2020-05-28 16:07:50 -0400

    fulfilling route to dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing'
+import Landing from './components/layout/Landing';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
 import { Provider } from 'react-redux';
@@ -35,6 +37,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register}/>
               <Route exact path='/login' component={Login}/>
+              <PrivateRoute exact path='/dashboard' component={Dashboard}/>
             </Switch>
           </section>
         </Fragment>
---
+++ client/src/components/dashboard/DashboardActions.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const DashboardActions = () => {
+    return (
+        <div className="dash-buttons">
+        <Link to="/edit-profile" className="btn btn-light"
+          ><i className="fas fa-user-circle text-primary"></i> Edit Profile</Link>
+        <Link to="/search-recipes" className="btn btn-light"
+          ><i className="fab fa-black-tie text-primary"></i> Search Recipes</Link>
+      </div>
+    )
+};
+
+export default DashboardActions
